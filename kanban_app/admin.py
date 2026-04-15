87d9c34def0932467bb979fb07d071ae27f0a10e
tranqn <namquoctran24@gmail.com>
2026-04-15 10:25:00 +0200

    feat(kanban_app): register Board, Task, Comment in admin
    - list_display, list_filter, search_fields per model

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,2 +1,30 @@
 # 2. Drittanbieter
 from django.contrib import admin
+
+# 3. Lokale Importe
+from .models import Board, Comment, Task
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    """Admin configuration for Board."""
+
+    list_display = ['id', 'title', 'owner']
+    search_fields = ['title', 'owner__email']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Admin configuration for Task."""
+
+    list_display = ['id', 'title', 'board', 'status', 'priority', 'assignee', 'reviewer']
+    list_filter = ['status', 'priority']
+    search_fields = ['title', 'board__title']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Admin configuration for Comment."""
+
+    list_display = ['id', 'task', 'author', 'created_at']
+    search_fields = ['author__email', 'task__title']
